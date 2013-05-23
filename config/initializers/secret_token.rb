c23a2c576761ed0f6600475bef0fb148f53c7672
Alexandria Ocasio <alex.ocasio@gmail.com>
2013-05-23 17:33:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'f533aacd2f02e58715484aa1f57033ee68d9a426cf03c6ac668f6cc56939dd2812a16bc99e06b4691b013ededbdf6592b7dac9d952b987db0c58f100788ee28c'
